6d6eeacb129782e9108000d4bce33f1d42638dad
kenmaready <ken@maready.com>
2020-05-22 08:28:40 -0400

    Initial commit (just a starting point)

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>welcome to funster...</h1>
+            </div>
+        )
+    }
+}
+
+export default home;
